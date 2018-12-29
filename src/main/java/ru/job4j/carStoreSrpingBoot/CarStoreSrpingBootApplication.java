3b1a6ef4c0ac621d9c0bd50299dbfb6fc0d002a0
Sergey11111 <sergey45684745@gmail.com>
2018-12-29 14:49:48 +0300

    Initial commit

---
+++ src/main/java/ru/job4j/carStoreSrpingBoot/CarStoreSrpingBootApplication.java
@@ -0,0 +1,14 @@
+package ru.job4j.carStoreSrpingBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStoreSrpingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarStoreSrpingBootApplication.class, args);
+	}
+
+}
+
